52c0d0edde8d48bdb38a18cde1a4f913a4e96032
marwannelsayed <marwanyehiaelsayed@gmail.com>
2023-05-26 23:12:24 +0300

    y

--- godfather-banks/src/App.js
+++ godfather-banks/src/App.js
@@ -1,10 +1,7 @@
 
 import Home from './Home';
-<<<<<<< HEAD
 import Banker from './Banker';
-=======
 import Login from  './Login';
->>>>>>> ea20d464023a5a31ea5aa30ddc0a28e25d02577b
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
  
@@ -17,13 +14,10 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-<<<<<<< HEAD
             <Route exact path="/Banker">
               <Banker />
-=======
             <Route exact path="/login">
               <Login />
->>>>>>> ea20d464023a5a31ea5aa30ddc0a28e25d02577b
             </Route>
           </Switch>
        </div>
