6c9f4310396e2cb642514811a32cb457b98120e2
mshereef03 <mshereef2003@gmail.com>
2023-05-28 19:05:19 +0300

    Added Client Home Page

--- godfather-banks/src/Client.js
+++ godfather-banks/src/Client.js
@@ -1,5 +1,11 @@
 import './Client.css';
-import logo from './nobg-logo.png';
+
+import creditCardIcon from './credit-card-icon.png';
+import loansIcon from './loans-icon.png';
+import transactionsIcon from './transaction-icon.png';
+import transfersIcon from './transfers-icon.png';
+import billsIcon from './bills-icon.png';
+
 import {Link} from 'react-router-dom';
 import Footer from './Footer.js';
 import Header from './Header.js';
@@ -10,11 +16,11 @@ const Client = () => {
     return (
         <>
             <div className="clientCards">
-                <button>Credit Cards</button>
-                <button>Loans</button>
-                <button>Transactions</button>
-                <button>Transfers</button>
-                <button>Bills</button>
+                <button><img src={creditCardIcon}/><br/>Credit Cards</button>
+                <button><img src={loansIcon}/><br/>Loans</button>
+                <button><img src={transactionsIcon}/><br/>Transactions</button>
+                <button><img src={transfersIcon}/><br/>Transfers</button>
+                <button><img src={billsIcon}/><br/>Bills</button>
             </div>
         </>
     );
