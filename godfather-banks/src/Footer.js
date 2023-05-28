41be2892980d1d3d5284835c840f747be9406ac5
mshereef03 <mshereef2003@gmail.com>
2023-05-28 16:21:06 +0300

    Made a footer component

---
+++ godfather-banks/src/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+
+function Footer() {
+    return (
+      <>
+         <footer className="home-footer">
+          <div className="home-footer-content">
+            <p>&copy; 2023 Godfather Banks. All rights reserved.</p>
+            <ul>
+              <li><a href="/privacy">Privacy Policy</a></li>
+              <li><a href="/terms">Terms of Service</a></li>
+            </ul>
+          </div>
+        </footer>
+      </>
+    );
+  }
+  
+  export default Footer;
