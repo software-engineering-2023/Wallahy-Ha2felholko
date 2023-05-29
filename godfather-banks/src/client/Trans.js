0162306925d509d3f0acc5851b45d8cc4bdb6e10
marwannelsayed <marwanyehiaelsayed@gmail.com>
2023-05-29 20:33:18 +0300

    Finalized Transactions Page

--- godfather-banks/src/client/Trans.js
+++ godfather-banks/src/client/Trans.js
@@ -3,6 +3,7 @@ import './Trans.css';
 
 function Trans() {
   const [selectedNotif, setSelectedNotif] = useState(null);
+  const [infoVisible2, setInfoVisible2] = useState(false);
   const [readNotifications, setReadNotifications] = useState([]);
   const [notifications, setNotifications] = useState([
     "Money credited to your account from a recent deposit of $500.",
@@ -34,6 +35,14 @@ function Trans() {
     }
   };
 
+  const handleButtonClick3 = () => {
+    setInfoVisible2(true);
+  };
+
+  const handleInfoClose3 = () => {
+    setInfoVisible2(false);
+  };
+
     return (
       <div className="Banker">
         <div className="body">
@@ -55,8 +64,31 @@ function Trans() {
                 <div className="SelectedNotif">
                   <h2>Transaction:</h2>
                   <p1>{selectedNotif}</p1>
-                  <h6>Sent: Saturday, May 27, 2023 11:36 AM</h6>
-                  <a className="UserInfoButton">Click here for more info</a>
+                  <h6>Sent: Saturday, May 15, 2023 11:36 AM</h6>
+                  {infoVisible2 && (
+                    <div className="info-tab2">
+                    <h3>Client's Info</h3>
+                    <p>• Transaction Date: May 15, 2023</p>
+                    <p>• Description: Grocery Store Purchase</p>
+                    <p>• Transaction Type: Debit</p>
+                    <p>• Amount: -$75.50</p>
+                    <p>• Account Balance: $1,250.25</p>
+                    <p>• Transaction Reference/ID: TRX789123456</p>
+                    <p>• Transaction Status: Completed</p>
+                    <p>• Transaction Category: Groceries</p>
+                    <p>• Payment Method: Debit Card</p>
+                    <p>• Payee/Payer: ABC Supermarket</p>
+                    <p>• Location: Citytown, USA</p>
+                    <p>• Fee or Charges: None</p>
+                    <p>• Running Balance: $1,250.25</p>
+                    <p>• Notes: Remember to track grocery expenses for monthly budgeting.</p>
+                    <p>• Statement Period: May 1, 2023 - May 31, 2023</p>
+                  <button className="CancelButton" onClick={handleInfoClose3}>
+                    Cancel
+                  </button>
+                </div>
+                )}
+                  <a className="UserInfoButton" onClick={handleButtonClick3}>Click here for detailed info</a>
                 </div>
             ) : (
               <p>Select a transaction to display</p>
